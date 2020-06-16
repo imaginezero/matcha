450845481f1f906abf7ffbf35aaea608de86d0d2
dmbch <daniel@dmbch.net>
2020-06-16 11:12:27 +0200

    feat: prepare enabling auth0 in production

--- utils/auth.js
+++ utils/auth.js
@@ -6,8 +6,8 @@ const auth = initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  redirectUri: `${process.env.VERCEL_URL}${process.env.AUTH0_REDIRECT_URI}`,
-  postLogoutRedirectUri: `${process.env.VERCEL_URL}${process.env.AUTH0_LOGOUT_URI}`,
+  redirectUri: `${process.env.VERCEL_URL}/api/auth/callback`,
+  postLogoutRedirectUri: `${process.env.VERCEL_URL}/api/prefs/reset`,
   session: {
     cookieSecret: process.env.AUTH0_COOKIE_SECRET,
     cookieLifetime: 60 * 60 * 24 * 7,
