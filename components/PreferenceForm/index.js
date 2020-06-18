eb8b6e3920f40328f9f64ad75ae0cf95d8fe70a3
dmbch <daniel@dmbch.net>
2020-06-18 23:38:20 +0200

    feat: style preferences page

--- components/PreferenceForm/index.js
+++ components/PreferenceForm/index.js
@@ -4,6 +4,22 @@ import { usePrefs } from '../../hooks';
 
 import { Checkbox } from '../Checkbox';
 
+import {
+  columns,
+  questionColumn,
+  checkboxColumn,
+  submitButton,
+} from './styles.module.css';
+
+const questions = {
+  isParent: 'Bist du Mutter oder Vater?',
+  isStudent: 'Gehst du zur Schule oder zur Uni?',
+  isScientist: 'Bist du Wissenschaftlerin oder Wissenschaftler?',
+  isOfficial: 'Repräsentierst du eine Behörde oder Kommune?',
+  isNgoExec: 'Repräsentierst du eine NGO oder eine aktivistische Gruppe?',
+  isCompanyExec: 'Repräsentierst du ein Unternehmen?',
+};
+
 export function PreferenceForm({ preferences }) {
   const { prefs, setPrefs, savePrefs } = usePrefs(preferences);
   const router = useRouter();
@@ -15,18 +31,30 @@ export function PreferenceForm({ preferences }) {
   return (
     <form onSubmit={handleSubmit} action="/api/prefs/update" method="POST">
       {prefs
-        ? Object.entries(prefs).map(([key, value], index) => (
-            <div key={index}>
-              <span>{key}</span>
-              <Checkbox
-                id={key}
-                checked={value}
-                onChange={() => setPrefs({ ...prefs, [key]: !value })}
-              />{' '}
+        ? Object.entries(prefs).map(([key, value]) => (
+            <div className={columns} key={key}>
+              <div
+                className={questionColumn}
+                onClick={() => setPrefs({ ...prefs, [key]: !value })}
+              >
+                <span>{questions[key]}</span>
+              </div>
+              <div className={checkboxColumn}>
+                <Checkbox
+                  id={key}
+                  checked={value}
+                  onChange={() => setPrefs({ ...prefs, [key]: !value })}
+                />
+              </div>
             </div>
           ))
         : null}
-      <input key="submit" type="submit" value="Speichern"></input>
+      <input
+        key="submit"
+        type="submit"
+        value="Einstellungen Speichern"
+        className={submitButton}
+      ></input>
     </form>
   );
 }
