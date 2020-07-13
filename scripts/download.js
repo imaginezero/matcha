a5be8b31ab49dafcbe2abb89049c58b0ed6a7bb8
dmbch <daniel@dmbch.net>
2020-07-13 15:00:49 +0200

    feat: add activity image download

--- scripts/download.js
+++ scripts/download.js
@@ -1,19 +1,54 @@
 require('dotenv').config();
 
 const { promisify } = require('util');
+const { basename, dirname, join } = require('path');
 const fs = require('fs');
+
+const rimraf = require('rimraf');
+const axios = require('axios');
+const pLimit = require('p-limit');
+const sharp = require('sharp');
+
+const limit = pLimit(2);
 
 const { getData, dataFile } = require('../utils/data');
 
+const imageFolder = join(dirname(__dirname), 'public', 'activities');
+
 const writeFile = promisify(fs.writeFile);
+const deleteFiles = promisify(rimraf);
 
 (async () => {
   try {
     const start = Date.now();
-    await writeFile(dataFile, JSON.stringify(await getData(true)));
-    console.log(`Success: activity data saved in ${Date.now() - start}ms.`);
+    const data = await getData(true);
+    await deleteFiles(dataFile);
+    await deleteFiles(join(imageFolder, '*'));
+    const activities = await Promise.all(
+      data.activities.map((activity) =>
+        limit(async () => {
+          const { slug, imgUrlInternal } = activity;
+          const fileName = `${slug}.jpg`;
+          const { data } = await axios.get(imgUrlInternal, {
+            responseType: 'arraybuffer',
+          });
+          await sharp(Buffer.from(data))
+            .resize({ width: 1200, height: 800 })
+            .jpeg({ quality: 75 })
+            .toFile(join(imageFolder, fileName));
+          return {
+            ...activity,
+            imgUrlPublic: `/${basename(imageFolder)}/${fileName}`,
+          };
+        })
+      )
+    );
+    await writeFile(dataFile, JSON.stringify({ ...data, activities }));
+    console.log(
+      `🎉 activity data and images saved in ${Date.now() - start}ms.`
+    );
   } catch (error) {
-    console.error(error);
+    console.error('💣', error);
     process.exit(1);
   }
 })();
