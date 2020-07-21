35943d4731d6ba7804d8bf8611e0878d723789dd
dmbch <daniel@dmbch.net>
2020-07-21 10:58:13 +0200

    feat: add tick marks to slider

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -1,18 +1,21 @@
 import { useEffort } from '../../hooks';
 
-import { slider } from './Slider.module.css';
+import { wrapper, slider, ticks } from './Slider.module.css';
 
 export default function Slider() {
   const { effort, setEffort } = useEffort();
   return (
-    <input
-      type="range"
-      min="10"
-      max="100"
-      step="10"
-      value={effort}
-      onChange={({ target: { value } }) => setEffort(value)}
-      className={slider}
-    />
+    <div className={wrapper}>
+      <input
+        type="range"
+        min="10"
+        max="100"
+        step="10"
+        value={effort}
+        onChange={({ target: { value } }) => setEffort(value)}
+        className={slider}
+      />
+      <div className={ticks} />
+    </div>
   );
 }
