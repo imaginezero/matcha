318db1ecab4ce8dd2cf5f31381e28e07e665a77b
dmbch <daniel@dmbch.net>
2020-07-30 15:48:55 +0200

    fix: make cookie banner work again as expected

--- hooks/useLogin.js
+++ hooks/useLogin.js
@@ -7,7 +7,7 @@ import {
 } from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
-const LoginContext = createContext(null);
+const LoginContext = createContext({});
 
 const fetchProfile = async () => {
   const response = await fetch('/api/auth/profile');
@@ -19,22 +19,43 @@ const fetchProfile = async () => {
 };
 
 export function withLogin(Component) {
-  const WrappedComponent = ({ profile: defaults = null, ...props }) => {
-    const parentContext = useContext(LoginContext);
-    const [profile, setProfile] = useState(defaults);
+  const WrappedComponent = ({ profile: serverSideProfile, ...props }) => {
+    // If we have determined during SSR that our user is not logged-in, mimic
+    // the fetchProfile()-call's corresponding return value. If, however, we
+    // have a truthy SSR injected profile use that, otherwise default to null.
+    const [profile, setProfile] = useState(
+      serverSideProfile === null ? { error: true } : serverSideProfile || null
+    );
+
+    // If we have a falsey profile state, set to null to signal the fact that
+    // login state is not yet determined. If we have a profile (or error
+    // response), use that to determine logged-in state.
     const isLoggedIn = !profile ? null : !profile.error;
-    let shouldFetch = true;
+
+    // If we have nested contexts, the child context is responsible for
+    // determining login state, since we are passing profile data to childs on
+    // some pages (esp. protected ones).
+    const { setParentProfile } = useContext(LoginContext);
+
+    // This variable will be changed by child components that call
+    // setParentProfile for this one.
+    let shouldFetch = isLoggedIn === null;
+
+    // Reminder: useEffect is called just once and child first, parent last.
     useEffect(() => {
-      if (parentContext) parentContext.setProfile(profile);
-      else if (shouldFetch) (async () => setProfile(await fetchProfile()))();
+      (async () => {
+        if (setParentProfile) setParentProfile(profile);
+        if (shouldFetch) setProfile(await fetchProfile());
+      })();
     }, []);
+
     return createElement(
       LoginContext.Provider,
       {
         value: {
           isLoggedIn,
           profile: isLoggedIn ? profile : null,
-          setProfile(profile) {
+          setParentProfile(profile) {
             shouldFetch = false;
             setProfile(profile);
           },
@@ -51,5 +72,6 @@ export function withLogin(Component) {
 }
 
 export function useLogin() {
-  return useContext(LoginContext);
+  const { isLoggedIn, profile } = useContext(LoginContext);
+  return { isLoggedIn, profile };
 }
