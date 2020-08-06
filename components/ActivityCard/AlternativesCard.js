9e0a5aca50bd4d576c1b0c7037207418a80ee3a7
dmbch <daniel@dmbch.net>
2020-08-06 14:20:15 +0200

    feat: tweak detail page whitespace

--- components/ActivityCard/AlternativesCard.js
+++ components/ActivityCard/AlternativesCard.js
@@ -5,10 +5,12 @@ import { useTranslation } from '../../hooks';
 import { Content } from '../Content';
 import { H3 } from '../Typo';
 
+import { alternativesWrapper } from './ActivityCard.module.css';
+
 export default function AlternativesCard({ activities }) {
   const { t } = useTranslation();
   return (
-    <Content>
+    <Content className={alternativesWrapper}>
       <H3>{t('activityAlternativesHeadline')}</H3>
       <p>{t('activityAlternativesDescription')}</p>
       <ul>
