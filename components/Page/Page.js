dece93ac599513b5bcedcac0e3526e95f0461f4c
dmbch <daniel@dmbch.net>
2020-08-11 18:21:01 +0200

    feat: add metadata for better sharing support

--- components/Page/Page.js
+++ components/Page/Page.js
@@ -1,20 +1,47 @@
 import Head from 'next/head';
 
-export default function Page({ title, description, metaData, children }) {
-  const ogData = { title, description, ...(metaData || {}) };
+import { getAbsoluteUrl } from '../utilities';
+
+export default function Page({
+  title,
+  description,
+  image = 'share-preview.jpg',
+  children,
+}) {
+  const imageUrl = getAbsoluteUrl(image);
   return (
     <>
       <Head>
-        <title>Matcha{title ? `: ${title}` : ''}</title>
-        {description ? (
-          <meta name="description" content={description} key="description" />
+        {/* Web / Google */}
+        <title>{title ? `Matcha: ${title}` : 'Matcha'}</title>
+        {description ? <meta name="description" content={description} /> : null}
+
+        {/* Facebook / OpenGraph */}
+        <meta property="og:site_name" content="Matcha" key="og:site_name" />
+        {title ? (
+          <meta property="og:title" content={title} key="og:title" />
         ) : null}
-        <meta property="og:site_name" content="Matcha" />
-        {Object.entries(ogData).map(([key, value]) => {
-          if (!value) return null;
-          const ogKey = `og:${key}`;
-          return <meta property={ogKey} content={value} key={ogKey} />;
-        })}
+        {description ? (
+          <meta
+            property="og:description"
+            content={description}
+            key="og:description"
+          />
+        ) : null}
+        <meta property="og:image:url" content={imageUrl} key="og:image:url" />
+        <meta property="og:image:width" content="900" key="og:image:width" />
+        <meta property="og:image:height" content="600" key="og:image:height" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@gomatcha_org" />
+        {title ? (
+          <meta name="twitter:title" content={title} key="twitter:title" />
+        ) : null}
+        {description ? (
+          <meta name="twitter:description" content={description} />
+        ) : null}
+        <meta name="twitter:image" content={imageUrl} />
       </Head>
       {children}
     </>
