e337abc5a5785c6b33ed12782de47bcc39f8aff7
Alexander Thiel <me@alexthiel.de>
2020-09-19 11:14:21 +0200

    refactor: removes invalid services export

--- services/index.js
+++ services/index.js
@@ -1,4 +1,3 @@
 export { getActivities, getActivity, recommendActivities } from './activity';
 export { getOrganizations, getOrganization } from './organization';
 export { getPages, getPage } from './page';
-export { EFFORT_STEP, EFFORT_MIN, EFFORT_MAX } from './static-config';
