4a8ab8c69eaa5a1570345f8623895c85f68ac0f1
Alexander Thiel <me@alexthiel.de>
2020-09-19 14:17:44 +0200

    fix: externalizes accidentally pushed strings

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -1,4 +1,5 @@
 import { useEffort } from '../../hooks';
+import { useTranslation } from '../../hooks';
 
 import {
   wrapper,
@@ -8,7 +9,10 @@ import {
 } from './Slider.module.css';
 
 export default function Slider() {
+
+  const { t } = useTranslation();
   const { effort, setEffort } = useEffort();
+
   return (
     <div className={wrapper}>
       <input
@@ -22,8 +26,8 @@ export default function Slider() {
       />
       <div className={ticks} />
       <div className={legend}>
-          <div>5 Minuten</div>
-          <div>Vollzeit</div>
+        <div>{t('mainSliderMinimum')}</div>
+        <div>{t('mainSliderMaximum')}</div>
       </div>
     </div>
   );
